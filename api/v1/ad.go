648614994bc85be8bb2fa6e32c451d996f703140
jimmyhealer <yahing6066@gmail.com>
2024-04-01 01:47:59 +0800

    feat: add getAds Request, add requestMessage handler

--- api/v1/ad.go
+++ api/v1/ad.go
@@ -1,7 +1,10 @@
 package v1
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jimmyhealer/ad-placement-service/models"
 )
 
 func CreateAd(c *gin.Context) {
@@ -11,8 +14,37 @@ func CreateAd(c *gin.Context) {
 }
 
 func GetAds(c *gin.Context) {
+	var getAdsRequestErrorMessages = map[string]string{
+		"Offset.min":     "offset must be greater than or equal to 1",
+		"Offset.max":     "offset must be less than or equal to 100",
+		"Limit.min":      "limit must be greater than or equal to 1",
+		"Limit.max":      "limit must be less than or equal to 100",
+		"Age.min":        "age must be greater than or equal to 1",
+		"Age.max":        "age must be less than or equal to 100",
+		"Gender.oneof":   "gender must be one of M, F",
+		"Country.oneof":  "country must be one of TW, JP",
+		"Platform.oneof": "platform must be one of android, ios, web",
+	}
+
+	type getAdsRequest struct {
+		Offset   int                   `form:"offset,default=0" binding:"omitempty,min=1,max=100"`
+		Limit    int                   `form:"limit,default=5" binding:"omitempty,min=1,max=100"`
+		Age      int                   `form:"age" binding:"omitempty,min=1,max=100"`
+		Gender   []models.GenderType   `form:"gender" binding:"omitempty,dive,oneof=M F"`
+		Country  []models.CountryCode  `form:"country" binding:"omitempty,dive,oneof=TW JP"`
+		Platform []models.PlatformType `form:"platform" binding:"omitempty,dive,oneof=android ios web"`
+	}
+
+	var req getAdsRequest
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": errorMessageHandler(err, getAdsRequestErrorMessages),
+		})
+		return
+	}
 
 	c.JSON(200, gin.H{
 		"message": "getAds",
+		"req":     req,
 	})
 }
---
+++ api/v1/utils.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func errorMessageHandler(err error, requestErrorMessages map[string]string) string {
+	if err == nil {
+		return ""
+	}
+
+	errs := err.(validator.ValidationErrors)
+	err_msgs := make([]string, 0)
+	for _, e := range errs {
+		// if field is array, need remove [*] from field name
+		field := e.Field()
+		if strings.Contains(field, "[") {
+			field = field[:strings.Index(field, "[")]
+		}
+		key := field + "." + e.Tag()
+		if msg, ok := requestErrorMessages[key]; ok {
+			err_msgs = append(err_msgs, msg)
+		} else {
+			err_msgs = append(err_msgs, e.Error())
+		}
+	}
+	return strings.Join(err_msgs, "; ")
+}
