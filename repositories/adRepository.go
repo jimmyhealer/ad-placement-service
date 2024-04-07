318996b82b2fa8c55f0bc92d529bf205efeae145
jimmyhealer <yahing6066@gmail.com>
2024-04-08 03:59:25 +0800

    fead: add removeBrackets

--- repositories/adRepository.go
+++ repositories/adRepository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/jimmyhealer/ad-placement-service/db"
@@ -35,6 +36,14 @@ func addCondition(query *gorm.DB, condition string, value interface{}, format st
 	return query
 }
 
+func removeBrackets(value []interface{}) string {
+	strSlice := make([]string, len(value))
+	for i, v := range value {
+		strSlice[i] = fmt.Sprint(v)
+	}
+	return strings.Trim(strings.Join(strSlice, "\",\""), "[]")
+}
+
 func (repo *ConcreteAdRepository) CreateAd(ad *models.Advertisement) error {
 	db := repo.db.GetDB()
 
@@ -50,25 +59,37 @@ func (repo *ConcreteAdRepository) FindActiveAds(
 	offset int,
 	limit int,
 	age int,
-	gender []models.GenderType,
-	country []models.CountryCode,
-	platform []models.PlatformType,
+	genders []models.GenderType,
+	countries []models.CountryCode,
+	platforms []models.PlatformType,
 ) ([]models.Advertisement, error) {
+	genderInterfaces := make([]interface{}, len(genders))
+	platformInterfaces := make([]interface{}, len(platforms))
+	countryInterfaces := make([]interface{}, len(countries))
+	for i, v := range genders {
+		genderInterfaces[i] = v
+	}
+	for i, v := range platforms {
+		platformInterfaces[i] = v
+	}
+	for i, v := range countries {
+		countryInterfaces[i] = v
+	}
+
 	db := repo.db.GetDB()
 
 	var ads []models.Advertisement
 
-	query := db.Joins("JOIN Conditions on Advertisements.ID = Conditions.advertisement_id")
+	query := db.Debug().Joins("JOIN Conditions on Advertisements.ID = Conditions.advertisement_id")
 
-	// TODO
-	nowTimeStr := nowTime.Format("2006-01-02 15:04:05")
+	nowTimeStr := nowTime.UTC().Format("2006-01-02 15:04:05+00")
 	query = addCondition(query, "start_at <= ?", nowTimeStr, "%s")
 	query = addCondition(query, "end_at >= ?", nowTimeStr, "%s")
 	query = addCondition(query, "age_start <= ?", age, "%d")
 	query = addCondition(query, "age_end >= ?", age, "%d")
-	query = addCondition(query, "gender @> ?", gender, `{"%s"}`)
-	query = addCondition(query, "country @> ?", country, `{"%s"}`)
-	query = addCondition(query, "platform @> ?", platform, `{"%s"}`)
+	query = addCondition(query, "gender @> ?", removeBrackets(genderInterfaces), `{"%s"}`)
+	query = addCondition(query, "country @> ?", removeBrackets(countryInterfaces), `{"%s"}`)
+	query = addCondition(query, "platform @> ?", removeBrackets(platformInterfaces), `{"%s"}`)
 
 	if err := query.Order("end_at ASC").Offset(offset).Limit(limit).Find(&ads).Error; err != nil {
 		return nil, err
