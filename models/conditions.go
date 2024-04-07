92af7ba4bbd700f45bff2708b54533b6c0827f27
jimmyhealer <yahing6066@gmail.com>
2024-04-08 05:45:33 +0800

    feat: add database index

--- models/conditions.go
+++ models/conditions.go
@@ -8,9 +8,9 @@ import (
 type Conditions struct {
 	gorm.Model
 	AdvertisementID uint
-	AgeStart        int            `form:"age_start" binding:"omitempty,min=1,max=100"`
-	AgeEnd          int            `form:"age_end" binding:"omitempty,min=1,max=100"`
-	Gender          pq.StringArray `gorm:"type:text[]" form:"gender" binding:"omitempty,dive,oneof=M F"`
-	Country         pq.StringArray `gorm:"type:text[]" form:"country" binding:"omitempty,dive,oneof=TW JP"`
-	Platform        pq.StringArray `gorm:"type:text[]" form:"platform" binding:"omitempty,dive,oneof=android ios web"`
+	AgeStart        int            `gorm:"index" form:"age_start" binding:"omitempty,min=1,max=100"`
+	AgeEnd          int            `gorm:"index" form:"age_end" binding:"omitempty,min=1,max=100"`
+	Gender          pq.StringArray `gorm:"type:text[];index" form:"gender" binding:"omitempty,dive,oneof=M F"`
+	Country         pq.StringArray `gorm:"type:text[];index" form:"country" binding:"omitempty,dive,oneof=TW JP"`
+	Platform        pq.StringArray `gorm:"type:text[];index" form:"platform" binding:"omitempty,dive,oneof=android ios web"`
 }
