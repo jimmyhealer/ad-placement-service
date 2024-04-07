e4f69edd59b1b218a3fad6717816cc8e1b5530ab
jimmyhealer <yahing6066@gmail.com>
2024-04-08 04:37:17 +0800

    rft: use t.run rewrite test

--- repositories/adRepository_test.go
+++ repositories/adRepository_test.go
@@ -16,29 +16,45 @@ func TestMain(m *testing.M) {
 }
 
 func TestIsNonEmptySlice(t *testing.T) {
-	// Test case 1: Empty slice
-	if isNonEmptySlice([]int{}) {
-		t.Errorf("Test case 1 Empty slice: Expected false, got true")
+	tests := []struct {
+		name     string
+		input    interface{}
+		expected bool
+	}{
+		{
+			name:     "Empty slice",
+			input:    []int{},
+			expected: false,
+		},
+		{
+			name:     "Non-empty slice",
+			input:    []int{1, 2, 3},
+			expected: true,
+		},
+		{
+			name:     "Non-slice value",
+			input:    10,
+			expected: true,
+		},
+		{
+			name:     "Empty array",
+			input:    [0]int{},
+			expected: false,
+		},
+		{
+			name:     "Non-empty array",
+			input:    [3]int{1, 2, 3},
+			expected: true,
+		},
 	}
 
-	// Test case 2: Non-empty slice
-	if !isNonEmptySlice([]int{1, 2, 3}) {
-		t.Errorf("Test case 2 Non-empty slice: Expected true, got false")
-	}
-
-	// Test case 3: Non-slice value
-	if !isNonEmptySlice(10) {
-		t.Errorf("Test case 3 Non-slice value: Expected true, got false")
-	}
-
-	// Test case 4: Empty array
-	if isNonEmptySlice([0]int{}) {
-		t.Errorf("Test case 4 Empty array: Expected false, got true")
-	}
-
-	// Test case 5: Non-empty array
-	if !isNonEmptySlice([3]int{1, 2, 3}) {
-		t.Errorf("Test case 5 Non-empty array: Expected true, got false")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := isNonEmptySlice(test.input)
+			if result != test.expected {
+				t.Errorf("Expected %v, got %v", test.expected, result)
+			}
+		})
 	}
 }
 
@@ -84,95 +100,94 @@ func TestFindActiveAds(t *testing.T) {
 
 	repo := NewAdRepository(db)
 
-	// Test case 1: Find active ads successfully
 	nowTime := time.Now()
 	offset := 0
 	limit := 5
-	age := 18
+	t.Run("Test case 1: Find active ads successfully", func(t *testing.T) {
+		age := 18
 
-	ads, err := repo.FindActiveAds(nowTime, offset, limit, age, nil, nil, nil)
+		ads, err := repo.FindActiveAds(nowTime, offset, limit, age, nil, nil, nil)
 
-	if err != nil {
-		t.Errorf("Test case 1: Expected nil, got %v", err)
-	}
-
-	if len(ads) != 2 {
-		t.Errorf("Test case 1: Expected 2 ads, got %d", len(ads))
-	}
-
-	for i := range ads {
-		if i > 0 && ads[i].EndAt.Before(ads[i-1].EndAt) {
-			t.Errorf("Test case 1: Ads are not sorted by end time")
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
 		}
-		if ads[i].StartAt.After(nowTime) || ads[i].EndAt.Before(nowTime) {
-			t.Errorf("Test case 1: Ad %d is not active", ads[i].ID)
+
+		if len(ads) != 2 {
+			t.Errorf("Expected 2 ads, got %d", len(ads))
 		}
-	}
 
-	// Test case 2: Find active ads with filter
-	age = 0
-	genders := []models.GenderType{"M"}
-	contries := []models.CountryCode{}
-	platforms := []models.PlatformType{}
-
-	ads, err = repo.FindActiveAds(nowTime, offset, limit, age, genders, contries, platforms)
-
-	if err != nil {
-		t.Errorf("Test case 2: Expected nil, got %v", err)
-	}
-
-	if len(ads) != 2 {
-		t.Errorf("Test case 2: Expected 2 ad, got %d", len(ads))
-	}
-
-	// Test case 3: Find no active ads with filter
-	genders = []models.GenderType{"F"}
-	contries = []models.CountryCode{"JP"}
-	platforms = []models.PlatformType{"web"}
-
-	ads, err = repo.FindActiveAds(nowTime, offset, limit, age, genders, contries, platforms)
-
-	if err != nil {
-		t.Errorf("Test case 3: Expected nil, got %v", err)
-	}
-
-	if len(ads) != 0 {
-		t.Errorf("Test case 3: Expected 0 ad, got %d", len(ads))
-	}
-
-	// Test case 4: Find no active ads with time filter
-	nowTime = time.Now().Add(-time.Hour * 96)
-	age = 18
-
-	ads, err = repo.FindActiveAds(nowTime, offset, limit, age, nil, nil, nil)
-
-	if err != nil {
-		t.Errorf("Test case 4: Expected nil, got %v", err)
-	}
-
-	if len(ads) != 0 {
-		t.Errorf("Test case 4: Expected 0 ad, got %d", len(ads))
-	}
-
-	// Test case 5: Check limit and offset
-	nowTime = time.Now().Add(time.Hour * 5)
-	offset = 6
-	limit = 3
-	age = 77
-
-	ads, err = repo.FindActiveAds(nowTime, offset, limit, age, nil, nil, nil)
-
-	if err != nil {
-		t.Errorf("Test case 5: Expected nil, got %v", err)
-	}
-
-	if len(ads) != 3 {
-		t.Errorf("Test case 5: Expected 3 ad, got %d", len(ads))
-	}
-
-	for i := range ads {
-		if ads[i].Title != fmt.Sprintf("Test loop-Ad-%02d", i+offset) {
-			t.Errorf("Test case 4: Ad %s is not correct", ads[i].Title)
+		for i := range ads {
+			if i > 0 && ads[i].EndAt.Before(ads[i-1].EndAt) {
+				t.Errorf("Ads are not sorted by end time")
+			}
+			if ads[i].StartAt.After(nowTime) || ads[i].EndAt.Before(nowTime) {
+				t.Errorf("Ad %d is not active", ads[i].ID)
+			}
 		}
-	}
+	})
+
+	t.Run("Test case 2: Find active ads with filter", func(t *testing.T) {
+		genders := []models.GenderType{"M"}
+		contries := []models.CountryCode{}
+		platforms := []models.PlatformType{}
+		ads, err := repo.FindActiveAds(nowTime, offset, limit, 0, genders, contries, platforms)
+
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+
+		if len(ads) != 2 {
+			t.Errorf("Expected 2 ads, got %d", len(ads))
+		}
+	})
+
+	t.Run("Test case 3: Find no active ads with filter", func(t *testing.T) {
+		age := 18
+		genders := []models.GenderType{"F"}
+		contries := []models.CountryCode{"JP"}
+		platforms := []models.PlatformType{"web"}
+		ads, err := repo.FindActiveAds(nowTime, offset, limit, age, genders, contries, platforms)
+
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+
+		if len(ads) != 0 {
+			t.Errorf("Expected 0 ads, got %d", len(ads))
+		}
+	})
+
+	t.Run("Test case 4: Find no active ads with time filter", func(t *testing.T) {
+		nowTime = time.Now().Add(-time.Hour * 96)
+		ads, err := repo.FindActiveAds(nowTime, offset, limit, 18, nil, nil, nil)
+
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+
+		if len(ads) != 0 {
+			t.Errorf("Expected 0 ads, got %d", len(ads))
+		}
+	})
+
+	t.Run("Test case 5: Check limit and offset", func(t *testing.T) {
+		nowTime = time.Now().Add(time.Hour * 5)
+		offset = 3
+		limit = 3
+		ads, err := repo.FindActiveAds(nowTime, offset, limit, 77, nil, nil, nil)
+
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+
+		if len(ads) != limit {
+			t.Errorf("Expected 3 ads, got %d", len(ads))
+		}
+
+		for i := range ads {
+			if ads[i].Title != fmt.Sprintf("Test loop-Ad-%02d", i+offset) {
+				t.Errorf("Ad %s is not correct", ads[i].Title)
+			}
+		}
+	})
 }
